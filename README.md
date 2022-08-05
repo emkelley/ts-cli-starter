a5a3fb09ca0c5adc760572a2def9380205af363f
Eric Kelley <hello@ericmkelley.com>
2022-08-04 21:30:54 -0400

    test

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# ts-cli-starter
+# Transmitty
+
+Transmitty is a Node app that will split a U.S. healthcare EFT/Payment Transmittal files, separate Checks and EFTs, and zip them for later processing.
+
+Transmitty was built to aid one of my work processes; Taking these often large 1,000+ page PDFs filled with mixed Checks and EFT payments, and processing them into a workable state for our Finance department. It was extremely tedious and time-consuming, so I automated it. If you want to use it yourself it will require some modification for your needs. Definitely not my best work but it gets the job done.
+
+![bJPTsBeewr](https://user-images.githubusercontent.com/11874169/164364244-7f127bae-149f-4c4c-b2ec-085f681d7477.png)
+
+## Usage
+
+0 - To use this utility, ensure your system has Node.js >= 16.x installed before continuing.
+
+1 - Install the app's dependencies: `yarn` or `npm install`
+
+2 - The source PDF to be processed is expected to be on the Desktop and be named `input.pdf`
+
+3 - Run the app: `yarn start` or `npm run start`
+
+4 - ???
+
+5 - Profit: Final output files will be placed on the Desktop in a folder called 'Transmitty Output'. The cmd window will close and the output folder will automatically be opened.
+### Tips
+
+For quick access, create a batch file on your Desktop that moves into the project directory and runs the npm command to start it. Something like this adapted for your use:
+```bat
+cd /D E:\Master Projects\transmitty
+yarn start
+```
---
+++ src/split.ts
@@ -0,0 +1,221 @@
+import { PDFDocument } from "pdf-lib";
+import figlet from "figlet";
+const fs = require("fs");
+const pdf = require("pdf-parse");
+const colors = require("colors");
+const rimraf = require("rimraf");
+const AdmZip = require("adm-zip");
+const homeDir = require("os").homedir();
+
+const outBase = "./out";
+const eftPath = "./out/eft";
+const chkPath = "./out/checks";
+const finalPath = `${homeDir}/Desktop/Transmitty Output`;
+
+const findInputFile = (directory: string) => {
+  const files = fs.readdirSync(directory);
+  const pdfFiles = files.filter((file: string) => file.endsWith(".pdf"));
+  for (const file of pdfFiles) {
+    const filePath = `${directory}/${file}`;
+    if (file.includes("Transmittal")) {
+      return filePath;
+    }
+  }
+};
+const inputFile = findInputFile(`${homeDir}/Desktop`);
+
+const splitPDFs = async (pathToPdf: string) => {
+  console.log(colors.cyan("Splitting PDF..."));
+  const docBytes = await fs.promises.readFile(pathToPdf);
+  const pdfDoc = await PDFDocument.load(docBytes);
+  const numberOfPages = pdfDoc.getPages().length;
+  console.log(`> Number of pages: ${numberOfPages}`);
+
+  for (let i = 0; i < numberOfPages; i++) {
+    process.stdout.write(`> Progress:  ${i}/${numberOfPages} \r`);
+    let fileName = "";
+    const subDoc = await PDFDocument.create();
+    const [copiedPage] = await subDoc.copyPages(pdfDoc, [i]);
+    subDoc.addPage(copiedPage);
+    const pdfBytes = await subDoc.save();
+
+    await pdf(pdfBytes).then(async (data: any) => {
+      const text = data.text.replace(".", "");
+      const textArr = text.split("\n");
+      const officeName = cleanFileName(`${textArr[8]}-${textArr[9]}`);
+
+      if (text.includes("Check #")) {
+        const checkNumber = Number(
+          text
+            .split("Check #:")[1]
+            .split(" ")[0]
+            .replace("Check", "")
+            .replace(".", "")
+        );
+        fileName = `Check-${checkNumber}-${officeName}`;
+      }
+
+      if (text.includes("EFT #")) {
+        const eftNumber = Number(
+          text
+            .split("EFT #:")[1]
+            .split(" ")[0]
+            .replace("Check", "")
+            .replace(".", "")
+        );
+        fileName = `EFT-${eftNumber}-${cleanFileName(officeName)}`;
+      }
+
+      await writePdfBytesToFile(
+        `tmp/${cleanFileName(fileName)}__${`${i + 1}`.padStart(5, "0")}.pdf`,
+        pdfBytes
+      );
+    });
+  }
+};
+
+const mergePDFs = async (tmp: string) => {
+  console.log("> Done Splitting PDFs...");
+  console.log(colors.cyan("Merging PDFs..."));
+
+  let chkCount = 0;
+
+  const files = fs.readdirSync(tmp);
+  const names = files.map((file: any) => file.slice(0, -4));
+
+  const checkNumbers = names.filter((file: any) => file.includes("Check-"));
+  const eftNumbers = names.filter((file: any) => file.includes("EFT-"));
+
+  const chkGrp = checkNumbers.reduce((acc: any, curr: any) => {
+    const checkNumber = curr.split("-")[1];
+    if (!acc[checkNumber]) acc[checkNumber] = [];
+    acc[checkNumber].push(curr);
+    return acc;
+  }, {});
+
+  const eftGroupsObj = eftNumbers.reduce((acc: any, curr: any) => {
+    const eftNumber = curr.split("-")[1];
+    if (!acc[eftNumber]) acc[eftNumber] = [];
+    acc[eftNumber].push(curr);
+    return acc;
+  }, {});
+
+  console.log(`> Checks Total: ${Object.keys(chkGrp).length}`);
+  console.log(`> EFTs Total: ${Object.keys(eftGroupsObj).length}`);
+
+  for (const key in chkGrp) {
+    if (chkGrp.hasOwnProperty(key)) {
+      chkCount++;
+
+      let names = chkGrp[key].sort();
+
+      let mergedPDF = await PDFDocument.create();
+
+      for (const name of names) {
+        const pdfFile = await PDFDocument.load(
+          fs.readFileSync(`${tmp}/${name}.pdf`)
+        );
+        const [copiedPage] = await mergedPDF.copyPages(
+          pdfFile,
+          pdfFile.getPageIndices()
+        );
+        mergedPDF.addPage(copiedPage);
+      }
+
+      const mergedPDFBytes = await mergedPDF.save();
+
+      await writePdfBytesToFile(`${chkPath}/${names[0]}.pdf`, mergedPDFBytes);
+      if (chkCount === Object.keys(chkGrp).length) {
+        await mergeAllChecks();
+      }
+    }
+  }
+
+  for (const key in eftGroupsObj) {
+    if (eftGroupsObj.hasOwnProperty(key)) {
+      let names = eftGroupsObj[key].sort();
+      let mergedPDF = await PDFDocument.create();
+
+      for (const fileName of names) {
+        const pdfFile = await PDFDocument.load(
+          fs.readFileSync(`${tmp}/${fileName}.pdf`)
+        );
+        const [copiedPage] = await mergedPDF.copyPages(
+          pdfFile,
+          pdfFile.getPageIndices()
+        );
+        mergedPDF.addPage(copiedPage);
+      }
+
+      const mergedPDFBytes = await mergedPDF.save();
+      await writePdfBytesToFile(`${eftPath}/${names[0]}.pdf`, mergedPDFBytes);
+    }
+  }
+};
+
+const mergeAllChecks = async () => {
+  console.log(colors.cyan("Merging checks..."));
+
+  await sleep(2000);
+
+  const files = fs.readdirSync(chkPath);
+
+  let mergedPDF = await PDFDocument.create();
+
+  for (const fileName of files) {
+    const pdfFile = await PDFDocument.load(
+      fs.readFileSync(`${chkPath}/${fileName}`)
+    );
+    const copiedPages = await mergedPDF.copyPages(
+      pdfFile,
+      pdfFile.getPageIndices()
+    );
+    copiedPages.forEach((page) => mergedPDF.addPage(page));
+  }
+
+  const finalPDF = await mergedPDF.save();
+
+  await writePdfBytesToFile(`${finalPath}/Checks_ALL.pdf`, finalPDF);
+
+  console.log(colors.green("--> Generated Checks_ALL.pdf"));
+};
+
+const zipFiles = async () => {
+  console.log(colors.yellow("Zipping files..."));
+
+  await sleep(2000);
+
+  const EFT_ZIP = new AdmZip();
+  const CHK_ZIP = new AdmZip();
+
+  CHK_ZIP.addLocalFolder(chkPath);
+  EFT_ZIP.addLocalFolder(eftPath);
+
+  EFT_ZIP.writeZip(`${finalPath}/EFTs.zip`);
+  CHK_ZIP.writeZip(`${finalPath}/Checks.zip`);
+
+  console.log(colors.green("--> Generated EFTs.zip"));
+  console.log(colors.green("--> Generated Checks.zip"));
+};
+
+const writePdfBytesToFile = async (fileName: string, pdfBytes: Uint8Array) => {
+  return fs.promises.writeFile(fileName, pdfBytes);
+};
+
+const cleanFileName = (fileName: string) => {
+  return fileName
+    .trim()
+    .replace("#", "")
+    .replace(".", "")
+    .replace(",", "")
+    .replace(/\s/g, "-");
+};
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+(async () => {
+  console.clear();
+  console.log(
+    colors.cyan(figlet.textSync("Transmitty", { horizontalLayout: "full" }))
+  );
+})();
